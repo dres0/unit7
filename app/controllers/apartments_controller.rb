bcafe2c3a99c942fa08695252239d5f51fe15893
Brayan Diaz C <brayandiazc@gmail.com>
2022-05-18 19:50:41 -0500

    feat. Apartments anidations

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -3,46 +3,50 @@ class ApartmentsController < ApplicationController
 
   # GET /apartments or /apartments.json
   def index
-    @apartments = Apartment.all
+    @building = Building.find params[:building_id]
+    @apartments = @building.apartments
   end
 
   # GET /apartments/1 or /apartments/1.json
   def show
+    @building = Building.find params[:building_id]
   end
 
   # GET /apartments/new
   def new
+    @building = Building.find params[:building_id]
     @apartment = Apartment.new
   end
 
   # GET /apartments/1/edit
   def edit
+    @building = Building.find params[:building_id]
+    @apartment = Apartment.find(params[:id])
   end
 
   # POST /apartments or /apartments.json
   def create
+    @building = Building.find params[:building_id]
     @apartment = Apartment.new(apartment_params)
 
+    @apartment.building = @building
     respond_to do |format|
       if @apartment.save
-        format.html { redirect_to apartment_url(@apartment), notice: "Apartment was successfully created." }
-        format.json { render :show, status: :created, location: @apartment }
+        format.html { redirect_to building_apartment_path(@building, @apartment), notice: 'Apartment was successfully created.' }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @apartment.errors, status: :unprocessable_entity }
+        format.html { render :new }
       end
     end
   end
 
   # PATCH/PUT /apartments/1 or /apartments/1.json
   def update
+    @building = Building.find params[:building_id]
     respond_to do |format|
-      if @apartment.update(apartment_params)
-        format.html { redirect_to apartment_url(@apartment), notice: "Apartment was successfully updated." }
-        format.json { render :show, status: :ok, location: @apartment }
+      if @apartment.update(apartment_params.merge(building: @building))
+        format.html { redirect_to building_apartment_path(@building, @apartment), notice: 'Apartment was successfully updated.' }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @apartment.errors, status: :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
@@ -52,7 +56,7 @@ class ApartmentsController < ApplicationController
     @apartment.destroy
 
     respond_to do |format|
-      format.html { redirect_to apartments_url, notice: "Apartment was successfully destroyed." }
+      format.html { redirect_to building_apartments_url, notice: "Apartment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,6 +69,6 @@ class ApartmentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def apartment_params
-      params.require(:apartment).permit(:number, :building_id)
+      params.require(:apartment).permit(:number)
     end
 end
